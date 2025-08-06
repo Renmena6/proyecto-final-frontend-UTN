b9fa0ba0d13147a7ea40d75b2bab9e6a749f6fa8
Renmena6 <renzomena6@gmail.com>
2025-08-06 18:15:18 -0300

    Agregue seccion de tecnologias utilizadas al about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,6 +17,14 @@ const About = () => {
             <p>Está dirigido a un público general interesado en la tecnología y las compras online. La interfaz ha sido diseñada para ser intuitiva y fácil de usar.</p>
         </section>
 
+        <section>
+            <ul>
+                <li><strong>React:</strong> Biblioteca principal pgitara construir la interfaz de usuario.</li>
+                <li><strong>FakeStoreAPI:</strong> API externa para obtener y gestionar los datos de los productos y usuarios.</li>
+                <li><strong>React Router DOM:</strong> SIrve para la navegación entre las diferentes páginas.</li>
+                <li><strong>CSS:</strong> Utilizado para dar estilo a la aplicación, con un enfoque en el diseño responsivo para adaptarse a distintos dispositivos.</li>
+            </ul>
+        </section>
     </Layout>
             );
 };
