e6a9dc8b90e23fd7c88818a4898153f622d6b008
Renmena6 <renzomena6@gmail.com>
2025-08-12 21:03:41 -0300

    About estilizado

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,31 +1,33 @@
 import { Layout } from "../components/Layout";
-
+import '../styles/pages/About.css';
 const About = () => {
     return (
 
-    <Layout>
-        <section>
-            <h1>Sobre Nosotros</h1>
-        
-            <h2>¿De qué trata este proyecto?</h2>
-            <p>Este proyecto es una tienda virtual construida con React que simula las funcionalidades básicas de un e-commerce. Permite a los usuarios explorar un catálogo de productos, buscar artículos específicos, y a los administradores (usuarios logueados) gestionar los productos, como actualizarlos o eliminarlos.</p>
-        </section>
+<Layout>
+            <div className="about-conteiner">
+                <section>
+                    <h1>Sobre Nosotros</h1>
+                    <h2>¿De qué trata este proyecto?</h2>
+                    <p>Este proyecto es una tienda virtual construida con React que simula las funcionalidades básicas de un e-commerce. Permite a los usuarios explorar un catálogo de productos, buscar artículos específicos, y a los administradores (usuarios logueados) gestionar los productos, como actualizarlos o eliminarlos.</p>
+                </section>
 
 
-        <section>
-            <h2>¿A quién está dirigido?</h2>
-            <p>Está dirigido a un público general interesado en la tecnología y las compras online. La interfaz ha sido diseñada para ser intuitiva y fácil de usar.</p>
-        </section>
+                <section>
+                    <h2>¿A quién está dirigido?</h2>
+                    <p>Está dirigido a un público general interesado en la tecnología y las compras online. La interfaz ha sido diseñada para ser intuitiva y fácil de usar.</p>
+                </section>
 
-        <section>
-            <ul>
-                <li><strong>React:</strong> Biblioteca principal pgitara construir la interfaz de usuario.</li>
-                <li><strong>FakeStoreAPI:</strong> API externa para obtener y gestionar los datos de los productos y usuarios.</li>
-                <li><strong>React Router DOM:</strong> SIrve para la navegación entre las diferentes páginas.</li>
-                <li><strong>CSS:</strong> Utilizado para dar estilo a la aplicación, con un enfoque en el diseño responsivo para adaptarse a distintos dispositivos.</li>
-            </ul>
-        </section>
-    </Layout>
+                <section>
+                    <h2>Tecnologías utilizadas</h2>
+                    <ul>
+                        <li><strong>React:</strong> Biblioteca principal para construir la interfaz de usuario.</li>
+                        <li><strong>FakeStoreAPI:</strong> API externa para obtener y gestionar los datos de los productos y usuarios.</li>
+                        <li><strong>React Router DOM:</strong> Sirve para la navegación entre las diferentes páginas.</li>
+                        <li><strong>CSS:</strong> Utilizado para dar estilo a la aplicación, con un enfoque en el diseño responsivo para adaptarse a distintos dispositivos.</li>
+                    </ul>
+                </section>
+            </div>
+        </Layout>
             );
 };
 
